e0d807acc451d360fa4b1eb0eb3ce54f08d201e5
Thiago Lopes <thiagoolopezz@gmail.com>
2024-04-22 22:54:44 -0300

    update

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
@@ -8,7 +7,6 @@ import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
 async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
 
   if (!res.ok) {
     throw new Error('Failed to fetch data')
@@ -32,7 +30,6 @@ export default async function Home() {
       <Skills />
       <Education />
       <Projects />
-      {/* <Blog blogs={blogs} /> */}
       <ContactSection />
     </>
   )
--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
-const path = require('path')
- 
+const path = require('path');
+
 module.exports = {
+  async headers() {
+    return [
+      {
+        // Define o favicon no cabeçalho HTML
+        source: '/app/favicon.ico',
+        headers: [
+          {
+            key: 'link',
+            value: '<link rel="icon" href="/app/favicon.ico" />',
+          },
+        ],
+      },
+    ];
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
     domains: ['res.cloudinary.com', 'media.dev.to']
   }
-}
+};
