723a42912f7a05afe4762ba08cea7dea79ff16ae
Thiago Lopes <thiagoolopezz@gmail.com>
2024-04-29 22:16:25 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,32 @@
-const nextConfig =  {
-  skipTrailingSlashRedirect: true,
-  trailingSlash: true,
-  output: 'export'
-}
+// const nextConfig =  {
+//   skipTrailingSlashRedirect: true,
+//   trailingSlash: true,
+//   output: 'export'
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const path = require('path');
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Define o favicon no cabeçalho HTML
+        source: '/app/favicon.ico',
+        headers: [
+          {
+            key: 'link',
+            value: '<link rel="icon" href="/app/favicon.ico" />',
+          },
+        ],
+      },
+    ];
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    domains: ['res.cloudinary.com', 'media.dev.to']
+  }
+};
