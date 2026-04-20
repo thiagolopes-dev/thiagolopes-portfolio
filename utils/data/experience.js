c94e0b4bb61174f8ea03cd6fd55dfa6ec1e890ed
Thiago Lopes <thiagoolopezz@gmail.com>
2026-04-20 10:42:31 -0300

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -2,19 +2,19 @@ export const experiences = [
   {
     id: 1,
     title: 'Programador Backend',
-    company: "Framework Nestjs",
+    company: "Frameworks: Nestjs, Nodejs",
     // duration: ""
   },
   {
     id: 2,
     title: "Programador Web Frontend",
-    company: "Framework Angular",
+    company: "Frameworks: Angular, Nextjs",
     // duration: ""
   },
   {
     id: 3,
     title: "Programador Mobile",
-    company: "Framework Ionic e Flutter",
+    company: "Frameworks: Ionic, Flutter, React Native",
     // duration: ""
   }
 ]
