5dd075a4cb9541a67d342b97e282fa674cd226aa
Thiago Lopes <thiagoolopezz@gmail.com>
2026-06-11 09:39:03 -0300

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,24 +1,30 @@
 export const educations = [
   {
     id: 1,
+    title: "Pós Graduação em Inteligência Artificial e Data Science",
+    duration: "2026",
+    institution: "Universidade Anhanguera",
+  },
+  {
+    id: 2,
     title: "Pós Graduação em Segurança de Redes",
     duration: "2024",
     institution: "Universidade Anhanguera",
   },
   {
-    id: 2,
+    id: 3,
     title: "Pós Graduação Desenvolvimento Web FullStack",
     duration: "2023",
     institution: "Universidade Anhanguera",
   },
   {
-    id: 3,
+    id: 4,
     title: "Pós Graduação em Desenvolvimento de Sistemas com Java",
     duration: "2018 - 2019",
     institution: "Faculdade Unyleya",
   },
   {
-    id: 4,
+    id: 5,
     title: "Curso de Graduação Superior de Tecnologia em Análise e Desenvolvimento de Sistemas",
     duration: "2014 - 2017",
     institution: "Faculdade Unopar",
