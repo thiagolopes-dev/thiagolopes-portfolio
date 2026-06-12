e2def0ef3a8aeb4da1f22597895d04b86ffc68fa
Thiago Lopes <thiagoolopezz@gmail.com>
2026-06-12 14:37:06 -0300

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Thiago Lopes",
   profile: '/thl.png',
-  designation: "Desenvolvedor de Software Full Stack",
+  designation: "Engenheiro de Software Full Stack",
   description: "Como programador fullstack e CEO da Daring Tech, TGMA e LabsDev, minha missão é desenvolver soluções tecnológicas que otimizam processos e impulsionam negócios. Minha experiência, comprovada em mais de 26 projetos reais entregues, abrange um stack robusto com Angular, NestJS, Docker, React Native, Nextjs, Ionic, Flutter, Python, Linux, MySQL, MongoDB e Inteligência Artificial. Atuo também como professor de Análise e Desenvolvimento de Sistemas na Unopar, compartilhando conhecimento e preparando novos profissionais para o mercado. Complementando minha atuação, sou responsável pelas Relações Institucionais na Ninhotech Governança de Inovação de Arapongas. Meu foco é transformar desafios em soluções eficientes, combinando tecnologia e estratégia para gerar impacto real.",
   email: 'thiago.lopes@thiagolopesdev.com.br',
   phone: '+5543999521823',
